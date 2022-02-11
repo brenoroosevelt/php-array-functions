67f18c02d32438eece09df29c4cd7d78623cb137
Breno Roosevelt <breno.jesus@ufms.br>
2022-02-11 14:01:57 -0400

    initial commit

---
+++ src/array_utils.php
@@ -0,0 +1,91 @@
+<?php
+declare(strict_types=1);
+
+namespace BrenoRoosevelt;
+
+function array_all(iterable $array, callable $callback): bool
+{
+    foreach ($array as $key => $value) {
+        if (true !== call_user_func_array($callback, [$value, $key])) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function array_some(iterable $array, callable $callback): bool
+{
+    return array_at_least($array, 1, $callback);
+}
+
+function array_none(iterable $array, callable $callback): bool
+{
+    return !array_some($array, $callback);
+}
+
+function array_at_least(iterable $array, int $n, callable $callback): bool
+{
+    $count = 0;
+    foreach ($array as $key => $value) {
+        if (true === call_user_func_array($callback, [$value, $key])) {
+            $count++;
+            if ($count >= $n) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+function array_at_most(iterable $array, int $n, callable $callback): bool
+{
+    $count = 0;
+    foreach ($array as $key => $value) {
+        if (true === call_user_func_array($callback, [$value, $key])) {
+            $count++;
+            if ($count > $n) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+function array_exactly(iterable $array, int $n, callable $callback): bool
+{
+    $count = 0;
+    foreach ($array as $key => $value) {
+        if (true === call_user_func_array($callback, [$value, $key])) {
+            $count++;
+            if ($count > $n) {
+                return false;
+            }
+        }
+    }
+
+    return $count === $n;
+}
+
+function array_first(iterable $array, callable $callback, $default = null): bool
+{
+    foreach ($array as $key => $value) {
+        if (true === call_user_func_array($callback, [$value, $key])) {
+            return $value;
+        }
+    }
+
+    return $default;
+}
+
+function array_each(iterable $array, callable $callback, bool $stoppable = true): void
+{
+    foreach ($array as $key => $value) {
+        $result = call_user_func_array($callback, [$value, $key]);
+        if ($result !== true && $stoppable) {
+            break;
+        }
+    }
+}
---
+++ tests/ArrayUtilsTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BrenoRoosevelt\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ArrayUtilsTest extends TestCase
+{
+    public function test_it_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
