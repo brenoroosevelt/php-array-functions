ada32b78246434b47c21c5f9dce1ac3d46ea07a7
brenoroosevelt <brenoroosevelt@users.noreply.github.com>
2022-02-13 12:40:19 +0000

    Fix styling

--- tests/IndexOfTest.php
+++ tests/IndexOfTest.php
@@ -3,106 +3,107 @@ declare(strict_types=1);
 
 namespace BrenoRoosevelt\Tests;
 
-use PHPUnit\Framework\TestCase;
 use function BrenoRoosevelt\index_of;
+use PHPUnit\Framework\TestCase;
 
 class IndexOfTest extends TestCase
 {
     public function indexOfProvider(): array
     {
         $anObject = new \stdClass();
+
         return [
             'case_1' => [
                 ['a', 'b', 'c'],
                 'b',
                 true,
-                1
+                1,
             ],
             'case_2' => [
-                (function(): \Generator {
+                (function (): \Generator {
                     yield 'a';
                     yield 'b';
                     yield 'c';
                 })(),
                 'b',
                 true,
-                1
+                1,
             ],
             'case_2_1' => [
-                (function(): \Generator {
+                (function (): \Generator {
                     yield 'x' => 'a';
                     yield 'y' => 'b';
                     yield 'z' => 'c';
                 })(),
                 'b',
                 true,
-                'y'
+                'y',
             ],
             'case_3' => [
                 new \ArrayObject(['a', 'b', 'c']),
                 'b',
                 true,
-                1
+                1,
             ],
             'case_4' => [
                 [1, 2, 3, 4],
                 '2',
                 false,
-                1
+                1,
             ],
             'case_4_1' => [
                 new \ArrayObject([1, 2, 3, 4]),
                 '2',
                 false,
-                1
+                1,
             ],
             'case_5' => [
                 [],
                 0,
                 true,
-                false
+                false,
             ],
             'case_5_1' => [
                 [],
                 0,
                 false,
-                false
+                false,
             ],
             'case_5_2' => [
                 new \ArrayObject([]),
                 null,
                 false,
-                false
+                false,
             ],
             'case_6' => [
                 [],
                 0,
                 true,
-                false
+                false,
             ],
             'case_7' => [
                 [1, 2, $anObject, 3],
                 $anObject,
                 true,
-                2
+                2,
             ],
             'case_8' => [
                 ['a' => 0, 'b' => null, 'c' => false],
                 null,
                 false,
-                'a'
+                'a',
             ],
             'case_8_1' => [
                 ['a' => 0, 'b' => null, 'c' => false],
                 null,
                 true,
-                'b'
+                'b',
             ],
             'case_8_2' => [
                 ['a' => 0, 'b' => null, 'c' => false],
                 false,
                 false,
-                'a'
+                'a',
             ],
         ];
     }
