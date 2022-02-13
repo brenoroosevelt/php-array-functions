20fa1991ad43a4a98fda3330a5b951b23b7ed4fc
Breno Roosevelt <breno.jesus@ufms.br>
2022-02-13 08:42:56 -0400

    tests

--- tests/ContainsTest.php
+++ tests/ContainsTest.php
@@ -3,123 +3,122 @@ declare(strict_types=1);
 
 namespace BrenoRoosevelt\Tests;
 
-use function BrenoRoosevelt\contains;
 use PHPUnit\Framework\TestCase;
+use function BrenoRoosevelt\contains;
 
 class ContainsTest extends TestCase
 {
     public function containsProvider(): array
     {
         $anObject = new \stdClass();
-
         return [
             'case_1' => [
                 ['a', 'b', 'c'],
                 'b',
                 true,
-                true,
+                true
             ],
             'case_2' => [
-                (function (): \Generator {
+                (function(): \Generator {
                     yield 'a';
                     yield 'b';
                     yield 'c';
                 })(),
                 'b',
                 true,
-                true,
+                true
             ],
             'case_2_1' => [
-                (function (): \Generator {
+                (function(): \Generator {
                     yield 'x' => 'a';
                     yield 'y' => 'b';
                     yield 'z' => 'c';
                 })(),
                 'b',
                 true,
-                true,
+                true
             ],
             'case_2_3' => [
-                (function (): \Generator {
+                (function(): \Generator {
                     yield 'x' => 'a';
                     yield 'y' => 'b';
                     yield 'z' => 'c';
                 })(),
                 'x',
                 true,
-                false,
+                false
             ],
             'case_3' => [
                 new \ArrayObject(['a', 'b', 'c']),
                 'b',
                 true,
-                true,
+                true
             ],
             'case_3_1' => [
                 new \ArrayObject(['a', 'b', 'c']),
                 0,
                 true,
-                false,
+                false
             ],
             'case_4' => [
                 [1, 2, 3, 4],
                 '2',
                 false,
-                true,
+                true
             ],
             'case_4_1' => [
                 new \ArrayObject([1, 2, 3, 4]),
                 '2',
                 false,
-                true,
+                true
             ],
             'case_5' => [
                 [],
                 0,
                 true,
-                false,
+                false
             ],
             'case_5_1' => [
                 [],
                 0,
                 false,
-                false,
+                false
             ],
             'case_5_2' => [
                 new \ArrayObject([]),
                 null,
                 false,
-                false,
+                false
             ],
             'case_6' => [
                 [],
                 0,
                 true,
-                false,
+                false
             ],
             'case_7' => [
                 [1, 2, $anObject, 3],
                 $anObject,
                 true,
-                true,
+                true
             ],
             'case_8' => [
                 ['a' => 0, 'b' => null, 'c' => false],
                 null,
                 false,
-                true,
+                true
             ],
             'case_8_1' => [
                 ['a' => 0, 'b' => null, 'c' => false],
                 null,
                 true,
-                true,
+                true
             ],
             'case_8_2' => [
                 ['b' => null, 'c' => false],
                 '',
                 false,
-                true,
+                true
             ],
         ];
     }
