ac95410e64beac0e548931398b8ac3123a15b21c
Breno Roosevelt <breno.jesus@ufms.br>
2022-02-15 09:16:17 -0400

    doc flatten

--- src/Flatten.php
+++ src/Flatten.php
@@ -3,23 +3,26 @@ declare(strict_types=1);
 
 namespace BrenoRoosevelt;
 
+use RecursiveArrayIterator;
+use RecursiveIteratorIterator;
+
 /**
  * @param array $items
- * @param string|null $separator
+ * @param string|null $pathSeparator
  * @return array
  */
-function flatten(array $items, ?string $separator = null)
+function flatten(array $items, ?string $pathSeparator = null): array
 {
     $result = [];
-    $iterator = new \RecursiveIteratorIterator(new \RecursiveArrayIterator($items));
+    $iterator = new RecursiveIteratorIterator(new RecursiveArrayIterator($items));
     foreach ($iterator as $leafValue) {
         $keys = [];
         foreach (range(0, $iterator->getDepth()) as $depth) {
             $keys[] = $iterator->getSubIterator($depth)->key();
         }
 
-        if (! empty($separator)) {
-            $result[join($separator, $keys) ] = $leafValue;
+        if (! empty($pathSeparator)) {
+            $result[join($pathSeparator, $keys) ] = $leafValue;
         } else {
             $result[] = $leafValue;
         }
